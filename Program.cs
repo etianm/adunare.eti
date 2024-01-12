eab154fa74970387cb6c13ae288a2c8f055faff3
etianm <155981372+etianm@users.noreply.github.com>
2024-01-13 00:42:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        Console.Write("Enter a: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter b: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(a + " + " + b + " = " + (a + b));
+    }
+}
+
